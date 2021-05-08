b557e26a359abbe50e9dcf7d6c17904e9f6b804f
NaFis Fuad Mim <nafisnfm@gmail.com>
2021-05-08 15:07:53 +0600

    addSomthing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,53 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const person = {
+    name:"mahfuz",
+    home:"bogura"
+  }
+  const person1 = {
+    name:"Nafis",
+    home:"setabgonj"
+  }
+  const style = {
+    color:"red"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
+      <h1 style={style}>{person1.name + " " + person1.home}</h1>
+      <h1 style ={{color:"darkBlue", backgroundColor:"black"}}>{person.name + " "+ person.home}</h1>
+      <h1>{(34+333)*56+45}</h1>
+      <h1>{3+4}</h1>
+      <Person10></Person10>
+      <Person10></Person10>
+      <Person10></Person10>
+      <Person10></Person10>
+      <Person10></Person10>
     </div>
   );
 }
+
+function Person10 () {
+  const textStyle = {
+    color:"tomato",
+    border:"1px solid gray",
+    margin:"5px",
+    padding:"5px"
+  }
+return (
+      <div style={textStyle}>
+        <h1>Name: NaFis Fuad Mim</h1>
+        <h3>Home: Bogura</h3>
+      </div>
+)
+}
+
+
+
+
+
+
+
+
 
 export default App;
