0b923d4c592b2050aee04efdc958d61847023d35
NaFis Fuad Mim <nafisnfm@gmail.com>
2021-05-09 09:23:00 +0600

    awsm

--- src/App.js
+++ src/App.js
@@ -2,53 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const person = {
-    name:"mahfuz",
-    home:"bogura"
-  }
-  const person1 = {
-    name:"Nafis",
-    home:"setabgonj"
-  }
-  const style = {
-    color:"red"
-  }
+  const productList = [
+    {name:"Sazna", price:"23"},
+    {name:"BorBoti", price:"45"},
+    {name:"Shim", price:"54"}
+  ]
+
+
+  const advanced = ['Murad','Maksim']
   return (
     <div className="App">
-      <h1 style={style}>{person1.name + " " + person1.home}</h1>
-      <h1 style ={{color:"darkBlue", backgroundColor:"black"}}>{person.name + " "+ person.home}</h1>
-      <h1>{(34+333)*56+45}</h1>
-      <h1>{3+4}</h1>
-      <Person10></Person10>
-      <Person10></Person10>
-      <Person10></Person10>
-      <Person10></Person10>
-      <Person10></Person10>
+      <Product sobzi={productList[0]}></Product>
+      <Product sobzi={productList[1]}></Product>
+      
+      <Player pukar="Afridi" jaiga="1B"></Player>
+      <Player pukar="Nafis" jaiga="2B"></Player>
+      <Player pukar={advanced[1]} jaiga="3B"></Player>
+      <Player pukar={advanced[0]} jaiga="4B"></Player>
+      
     </div>
   );
-}
-
-function Person10 () {
-  const textStyle = {
-    color:"tomato",
-    border:"1px solid gray",
-    margin:"5px",
-    padding:"5px"
   }
-return (
-      <div style={textStyle}>
-        <h1>Name: NaFis Fuad Mim</h1>
-        <h3>Home: Bogura</h3>
-      </div>
-)
+
+function Product (props) {
+  const {name,price} = props.sobzi;
+  console.log(props)
+  const ProductStyle = {
+    border:'3px solid gray'
+  }
+  return(
+    <div style={ProductStyle}>
+        <h1>Name: {name}</h1>
+        <h3>Price:{price}</h3>
+    </div>
+  )
 }
 
+function Player (props) {
+  return(
+    <div>
+      <h1>Name: {props.pukar}</h1>
+      <h2>Position: {props.jaiga}</h2>
+      
 
-
-
-
-
-
-
+    </div>
+  )
+}
 
 export default App;
